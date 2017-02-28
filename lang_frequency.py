a84f94d0ad3cd838bf17656fa17d45de2a0b8c26
bulbule <anyagoremykina@gmail.com>
2017-02-28 12:17:09 +0100

    load_data only reads the file, use argparse
    also new constant for a number of common words to output

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,6 +2,7 @@ import os
 import sys
 import re
 from collections import Counter
+import argparse
 
 
 def load_data(filepath):
@@ -9,26 +10,26 @@ def load_data(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath, "r") as data_file:
-        return data_file.read().lower()
+        return data_file.read()
 
 
-def get_most_frequent_words(text):
-    ''' Finds 10 most common words '''
+def get_most_frequent_words(text, first_n_words):
 
     # to count non-ASCII letters as well
     words = re.findall(r'\b[^\W\d_]+\b', text, re.I)
-    return Counter(words).most_common(10)
+    return Counter(words).most_common(first_n_words)
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) == 1:
-        sys.exit('Usage: python lang_frequency.py < filepath >')
-    else:
-        your_text = load_data(sys.argv[1])
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+    args = parser.parse_args()
+    your_text = load_data(args.filepath)
     if not your_text:
-        sys.exit('Error: the file %s was not found!' % sys.argv[1])
+        sys.exit('Error: the file %s was not found!' % args.filepath)
     else:
-        for a_word in get_most_frequent_words(your_text):
+        first_n_words = 10
+        for a_word in get_most_frequent_words(
+                your_text.lower(), first_n_words):
             print(a_word[0])
